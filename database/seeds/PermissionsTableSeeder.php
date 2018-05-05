2e07875cca360bab48d6eedffce2631cb427e60a
Marcelo Nogueira <marcelon27@gmail.com>
2018-05-04 22:21:42 -0300

    deficição das permissões

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Permission;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -11,6 +12,91 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Users
+        Permission::create([
+            'name'        => 'Navegar usuarios',
+            'slug'        => 'users.index',
+            'description' => 'lista e navega por todos usuarios do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalhes dos usuarios',
+            'slug'        => 'users.show',
+            'description' => 'ver em detalhes cada usuario do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edição de usuarios',
+            'slug'        => 'users.edit',
+            'description' => 'Editar qualquer dado de um usuário do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar usuarios',
+            'slug'        => 'users.destroy',
+            'description' => 'Eliminar qualquer dado de um usuarios do sistema'
+        ]);
+
+        // Roles
+        Permission::create([
+            'name'        => 'Navegar roles',
+            'slug'        => 'roles.index',
+            'description' => 'lista e navega por todos roles do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalhes dos rol',
+            'slug'        => 'roles.show',
+            'description' => 'ver em detalhes cada roles do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edição de rol',
+            'slug'        => 'roles.create',
+            'description' => 'Editar qualquer dado de um roles do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edição de rol',
+            'slug'        => 'roles.edit',
+            'description' => 'Editar qualquer dado de um roles do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar rol',
+            'slug'        => 'roles.destroy',
+            'description' => 'Eliminar qualquer dado de um roles do sistema'
+        ]);
+
+        // Products
+        Permission::create([
+            'name'        => 'Navegar products',
+            'slug'        => 'products.index',
+            'description' => 'lista e navega por todos products do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalhes dos product',
+            'slug'        => 'products.show',
+            'description' => 'ver em detalhes cada products do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edição de product',
+            'slug'        => 'products.create',
+            'description' => 'Editar qualquer dado de um products do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edição de product',
+            'slug'        => 'products.edit',
+            'description' => 'Editar qualquer dado de um products do sistema'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar product',
+            'slug'        => 'products.destroy',
+            'description' => 'Eliminar qualquer dado de um products do sistema'
+        ]);
     }
 }
